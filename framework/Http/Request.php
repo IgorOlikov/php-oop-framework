60dec003c46beba650d7a753f419f479a48d5440
IgorOlikov <i.olikov@mail.ru>
2023-12-18 11:45:22 +0500

    composer

---
+++ framework/Http/Request.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Framework\Http;
+
+class Request
+{
+public function createFromGlobals()
+{
+  echo 'STRIIING';
+}
+
+
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo 'hello world';
+require_once dirname(__DIR__).'/vendor/autoload.php';
+//require_once __DIR__.'/../vendor/autoload.php';
+
+
+
+use Framework\Http\Request;
+
+
+$aaa = new Request();
+//$bbb = new TestClass();
+
+phpinfo();
+
+dd(dirname(__DIR__).'/vendor/autoload.php');
+//dd($bbb->test());
+dd($aaa->createFromGlobals());
+
