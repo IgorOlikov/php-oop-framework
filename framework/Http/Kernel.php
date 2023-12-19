1b4d0a7d557136174b7bf9b98cea6b5525e94714
IgorOlikov <i.olikov@mail.ru>
2023-12-19 11:10:31 +0500

    Router class

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -3,41 +3,28 @@
 namespace Framework\Http;
 
 use FastRoute\RouteCollector;
+use Framework\Routing\RouterInterface;
 use function FastRoute\simpleDispatcher;
 use Framework\Http\Response;
 class Kernel
 {
+    public function __construct(private RouterInterface $router)
+    {
+
+    }
+
+
 
    public function handle(Request $request): Response
    {
-       $dispatcher  = simpleDispatcher(function (RouteCollector $collector){
-             $routes = include BASE_PATH.'/routes/web.php';
-            //dd($routes);
-            foreach ($routes as $route){
-                $collector->addRoute(...$route);
-            }
+       try {
+           [$routeHandler, $vars] = $this->router->dispatch($request);
 
-          //$collector->get('/', function() {
-          //     $content = 'HELLO WORLD3 HTML1111';
-          //     return new Response($content);
-          // });
-          //$collector->get('/posts/{id:\d+}', function (array $vars){
-          //    $content = "POST - {$vars['id']}";
-          //    return new Response($content);
-          // });
-        });
-
-       //dd($request->getPath());
-
-
-      $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
-
-      [$status,[$controller,$method],$vars] = $routeInfo;
-
-      $response = call_user_func_array([new $controller,$method],$vars);
-
+           $response = call_user_func_array($routeHandler, $vars);
+       }catch (\Throwable $exception){
+           $response = new Response($exception->getMessage(),statusCode: 500);
+       }
       return $response;
-
-   }
+    }
 
 }
--- public/index.php
+++ public/index.php
@@ -6,13 +6,15 @@ require_once BASE_PATH.'/vendor/autoload.php';
 
 use Framework\Http\Kernel;
 use Framework\Http\Request;
-
+use Framework\Routing\Router;
 
 $request = Request::createFromGlobals();
 
 
+$router = new Router();
 
-$kernel = new Kernel();
+
+$kernel = new Kernel($router);
 $response = $kernel->handle($request);
 
 
