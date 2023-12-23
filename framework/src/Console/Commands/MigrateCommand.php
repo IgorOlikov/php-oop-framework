0420157a20a03b76163e3148a24492fe9d5d7fdb
IgorOlikov <i.olikov@mail.ru>
2023-12-23 09:19:16 +0500

    get applied migrations

--- framework/src/Console/Commands/MigrateCommand.php
+++ framework/src/Console/Commands/MigrateCommand.php
@@ -10,7 +10,6 @@ use Framework\Console\CommandInterface;
 class MigrateCommand implements CommandInterface
 {
     private string $name = 'migrate';
-
     private const MIGRATIONS_TABLE = 'migrations';
     public function __construct(
         private Connection $connection
@@ -20,23 +19,31 @@ class MigrateCommand implements CommandInterface
 
     public function execute(array $parameters = []): int
     {
+        try {
+            $this->createMigrationsTable();
+
+            $this->connection->beginTransaction();
+
+            $appliedMigrations = $this->getAppliedMigrations();
+
+            dd($appliedMigrations);
+
+            $this->connection->commit();
+        }catch (\Throwable $e) {
+            $this->connection->rollBack();
+            throw $e;
+        }
 
 
-
-        $this->createmigrationsTable();
-
-
-
+        //$this->createmigrationsTable();
 
         return 0;
     }
-    private function createmigrationsTable(): void
+    private function createMigrationsTable(): void
     {
         $schemaManager = $this->connection->createSchemaManager();
 
         if (!$schemaManager->tablesExist(self::MIGRATIONS_TABLE)) {
-
-
             $schema = new Schema();
             $table = $schema->createTable(self::MIGRATIONS_TABLE);
             $table->addColumn('id', Types::INTEGER, [
@@ -48,12 +55,22 @@ class MigrateCommand implements CommandInterface
                 'default' => 'CURRENT_TIMESTAMP'
             ]);
             $table->setPrimaryKey(['id']);
-
             $sqlArray = $schema->toSql($this->connection->getDatabasePlatform());
-
             $this->connection->executeQuery($sqlArray[0]);
-
             echo 'Table migrations has been created' . PHP_EOL;
         }
+    }
+    private function getAppliedMigrations()
+    {
+        $queryBuilder =  $this->connection->createQueryBuilder();
+
+        return $queryBuilder
+             ->select('migration')
+             ->from(self::MIGRATIONS_TABLE)
+             ->executeQuery()
+             ->fetchFirstColumn();
+
+
+
     }
 }
