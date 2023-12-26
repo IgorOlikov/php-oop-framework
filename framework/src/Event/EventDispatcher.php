28553cd57b7c2b958e6d542cd716478476950f93
IgorOlikov <i.olikov@mail.ru>
2023-12-26 11:14:15 +0500

    psr event-dispatcher

---
+++ framework/src/Event/EventDispatcher.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Framework\Event;
+
+use Psr\EventDispatcher\EventDispatcherInterface;
+
+class EventDispatcher implements EventDispatcherInterface
+{
+    private array $listeners = [];
+
+    public function dispatch(object $event)
+    {
+        foreach ($this->getListenersForEvent($event) as $listener){
+            $listener($event);
+        }
+    }
+
+
+    public function getListenersForEvent(object $event):iterable
+    {
+        return [];
+    }
+}
